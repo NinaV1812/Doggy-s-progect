c94b8d55b571a340a55b83e37c5e0733359e9e07
Bloodorange690 <bluespruce185@gmail.com>
2021-01-13 18:44:39 +0100

    The app fetches right breeds from api

--- src/components/DogsLayout.js
+++ src/components/DogsLayout.js
@@ -12,17 +12,21 @@ const DogsLayout = () => {
   const breedy = useSelector(selectBreed);
   // const {breedy} = useContext(CompactMode)
   // const { isCompact } = useContext(CompactMode);
-  const param = breedy[0] ? breedy[0] : "beagle";
+  const param = breedy[0]
+    ? breedy[0].split(" ")[0].replace(",", "").toLowerCase()
+    : "beagle";
 
   const [items, setItems] = useState({
     items: Array.from({ length: 20 }),
     hasMore: true,
   });
   console.log("breed in dogs layout", breedy);
+  console.log("param ", param);
+
 
   useEffect(() => {
     const fetchData = async () => {
-      console.log("param ", param);
+      console.log("param in useeffect ", param);
 
       const result = await axios(`https://dog.ceo/api/breed/${param}/images`);
 
@@ -35,7 +39,7 @@ const DogsLayout = () => {
     };
 
     fetchData();
-  }, []);
+  }, [param]);
 
   const clichHandel = () => {
     const fetchData = async () => {
@@ -50,11 +54,11 @@ const DogsLayout = () => {
     fetchData();
   };
 
-  if (dogsPictures) {
+  if (breedy[0]) {
     return (
       <div class="container">
         <div>
-          <button onClick={clichHandel}>Show me more dogs</button>
+          {/* <button onClick={clichHandel}>Show me more dogs</button> */}
         </div>
         {/* <div class="col-lg-4 col-sm-6 thumbnail"> */}
         <div id="photos">
@@ -73,7 +77,10 @@ const DogsLayout = () => {
       </div>
     );
   } else {
-    return <p>Loading</p>;
+    return(<div> <p>Loading</p> 
+              {/* <button onClick={clichHandel}>Show me more dogs</button> */}
+
+    </div>);
   }
 };
 export default DogsLayout;
