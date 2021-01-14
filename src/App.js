c63eb697791fbb58931af188f60ebfc461a91373
Bloodorange690 <bluespruce185@gmail.com>
2021-01-14 17:24:56 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import DogsLayout from "./components/DogsLayout";
-import UploadImage from "../src/components/Uploadddd";
+import UploadImage from "./components/UploadAndClassify";
 
 function App() {
   return (
     <div>
+      <div>
       <UploadImage />
+      </div>
+      <div>
       <DogsLayout />
+    </div>
     </div>
   );
 }
